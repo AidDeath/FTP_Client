5fd64d57860460c377a5820f7e98438a15ccd8c1
Aid Death <aiddeath@gmail.com>
2019-08-15 12:00:38 +0300

    Add project files.

---
+++ FTP_try_1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace FTP_try_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox_pwdsave = new System.Windows.Forms.CheckBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.textBox_pwd = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.textBox_hostname = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox_pwdsave);
+            this.groupBox1.Controls.Add(this.button_connect);
+            this.groupBox1.Controls.Add(this.textBox_pwd);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Controls.Add(this.textBox_port);
+            this.groupBox1.Controls.Add(this.textBox_hostname);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(724, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подключение к FTP";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(294, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Пароль:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(294, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Порт";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Имя хоста \\ IP адрес";
+            // 
+            // checkBox_pwdsave
+            // 
+            this.checkBox_pwdsave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox_pwdsave.AutoSize = true;
+            this.checkBox_pwdsave.Location = new System.Drawing.Point(504, 45);
+            this.checkBox_pwdsave.Name = "checkBox_pwdsave";
+            this.checkBox_pwdsave.Size = new System.Drawing.Size(159, 17);
+            this.checkBox_pwdsave.TabIndex = 5;
+            this.checkBox_pwdsave.Text = "Сохранять логин и пароль";
+            this.checkBox_pwdsave.UseVisualStyleBackColor = true;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_connect.Location = new System.Drawing.Point(504, 16);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(159, 23);
+            this.button_connect.TabIndex = 4;
+            this.button_connect.Text = "Подключить";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.Button_connect_Click);
+            // 
+            // textBox_pwd
+            // 
+            this.textBox_pwd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_pwd.Location = new System.Drawing.Point(349, 42);
+            this.textBox_pwd.MaximumSize = new System.Drawing.Size(128, 20);
+            this.textBox_pwd.Name = "textBox_pwd";
+            this.textBox_pwd.Size = new System.Drawing.Size(128, 20);
+            this.textBox_pwd.TabIndex = 3;
+            this.textBox_pwd.UseSystemPasswordChar = true;
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_login.Location = new System.Drawing.Point(349, 20);
+            this.textBox_login.MaximumSize = new System.Drawing.Size(128, 20);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(128, 20);
+            this.textBox_login.TabIndex = 2;
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(172, 42);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(64, 20);
+            this.textBox_port.TabIndex = 1;
+            // 
+            // textBox_hostname
+            // 
+            this.textBox_hostname.Location = new System.Drawing.Point(6, 42);
+            this.textBox_hostname.Name = "textBox_hostname";
+            this.textBox_hostname.Size = new System.Drawing.Size(160, 20);
+            this.textBox_hostname.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 389);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(724, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(106, 17);
+            this.toolStripStatusLabel1.Text = "Нет подключения";
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 411);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(740, 450);
+            this.Name = "Form1";
+            this.Text = "Simple FTP Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox_pwdsave;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.TextBox textBox_pwd;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.TextBox textBox_hostname;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+    }
+}
+
---
+++ FTP_try_1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FTP_try_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_hostname.Text = Properties.Settings.Default.Hostname;
+            textBox_port.Text = Properties.Settings.Default.Port;  // Загружаем сохранённые с прошлого раза данные
+            textBox_login.Text = Properties.Settings.Default.Login;
+            textBox_pwd.Text = Properties.Settings.Default.Pwd;
+            if (Properties.Settings.Default.Savepwd == true)
+            {
+                checkBox_pwdsave.Checked = true;
+            }
+        }
+
+        private void Button_connect_Click(object sender, EventArgs e)
+        {
+
+
+            Properties.Settings.Default.Hostname = textBox_hostname.Text;
+            Properties.Settings.Default.Port = textBox_port.Text;
+            Properties.Settings.Default.Login = textBox_login.Text;
+                if (checkBox_pwdsave.Checked)
+                {
+                Properties.Settings.Default.Pwd = textBox_pwd.Text;
+                Properties.Settings.Default.Savepwd = true;
+                }
+             else
+                {
+                Properties.Settings.Default.Pwd = "";
+                Properties.Settings.Default.Savepwd = false;
+                }
+            Properties.Settings.Default.Save();   // Сохраняем данные, чтобы не вводить заново
+
+        }
+    }
+}
